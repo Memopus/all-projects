3c75de97dca4ec902003dff96fdbb271bd993cd0
Memopus <yassinesaq2007@gmail.com>
2023-09-25 22:06:52 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>Html css & js</h2>
+      <ol>
+        <li>
+          <a href="https://github.com/Memopus/odin-recipes">
+            Project : Recipes
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Memopus/odin-landing-page">
+            Landing Page
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Memopus/rock-paper-scissors">
+            Rock Paper Scissors
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Memopus/etch-a-sketch">Etch a sketch</a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://github.com/Memopus/calculator">Calculator</a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://github.com/Memopus/form">Sign Up Form</a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://github.com/Memopus/dashboard">Admin Dashboard</a>
+        </li>
+        <li>
+          <a href="https://github.com/Memopus/library">Library</a>{" "}
+        </li>
+        <li>
+          {" "}
+          <a href="https://github.com/Memopus/TicTacToe">Tic Tac Toe</a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://github.com/Memopus/restaurant-page">
+            Restaurant Page
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://github.com/Memopus/todo-app">ToDo List</a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://github.com/Memopus/weather">Weather App</a>
+        </li>
+        <li>
+          <a href="https://github.com/Memopus/battleship">BattleShip</a>
+        </li>
+      </ol>
+
+      <h2>Algorithms : </h2>
+
+      <ol>
+        <li>
+          <a href="https://github.com/Memopus/mergeSort">Recursion</a>{" "}
+        </li>
+        <li>
+          <a href="https://github.com/Memopus/linked-list">Linked List</a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://github.com/Memopus/BST">Binary Search tree</a>
+        </li>
+        <li>
+          <a href="https://github.com/Memopus/knightMoves">Knigh Travails</a>
+        </li>
+      </ol>
+
+      <h2>React : </h2>
+      <ol>
+        <li>
+          {" "}
+          <a href="https://github.com/Memopus/CV-BUILDOR">Cv application</a>
+        </li>
+        <li>
+          <a href="https://github.com/Memopus/memory-card-game">
+            Memory Card Game
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Memopus/shopping-cart">Shopping Cart</a>
+        </li>
+      </ol>
+
+      <h2>Node js : </h2>
+      <ol>
+        <li>
+          <a href="https://github.com/Memopus/basic-info-website">
+            Basic informational website
+          </a>
+        </li>
+      </ol>
+    </>
+  );
+}
+
+export default App;
